f82746a8e20c1558361875d24eaf658deb89bfa7
Juan Jesus Ulises Andrade Torres <juan24472@cbtis75.edu.mx>
2026-09-22 08:00:30 -0600

    Implementa mensajes con SweetAlert2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,33 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   imports: [RouterOutlet],
   selector: 'app-root',
-  styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
   protected readonly title = signal('youtube-app');
+
+  buscar(input: HTMLInputElement) {
+    const texto = input.value.trim();
+
+    if (!texto) {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Búsqueda vacía',
+        text: 'Escribe algo para buscar videos.',
+        confirmButtonText: 'Aceptar'
+      });
+      return;
+    }
+
+    Swal.fire({
+      icon: 'info',
+      title: 'Buscando...',
+      text: `Buscaremos videos de: ${texto}`,
+      confirmButtonText: 'Aceptar'
+    });
+  }
 }
